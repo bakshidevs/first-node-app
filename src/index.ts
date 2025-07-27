2cea884bebb270f4d7ff127b3c48007b1c6fb979
bakshidevs <sunandasamantaofficial@gmail.com>
2025-07-27 22:50:04 +0530

    set up done for react

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import cors from "cors";
+
 
 const app = express()
 const port = 8000
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
+
+
+app.get('/api/v1', (req, res) => {
+    res.json({
+        message: "Hello, React. I am from Express!"
+    })
 })
 
 
